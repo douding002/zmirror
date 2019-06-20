433958f622611c96a18db64f351fd5622d3839da
douding002 <douding002@126.com>
2019-06-20 21:33:28 +0800

    update debug

--- debug_clean.sh
+++ debug_clean.sh
@@ -2,3 +2,5 @@ sudo rm -r error_dump
 sudo rm -r __pycache__ 
 sudo rm automatic_domains_whitelist.log 
 rm config.py
+rm config.py.txt
+rm wsgi.py.txt
--- debug_run.sh
+++ debug_run.sh
@@ -1,6 +1,8 @@
 sudo kill -9 $(ps -ef|grep "python3 wsgi.py"|grep -v grep|awk '{print $2}')
 
 cp config_default.py config.py
+cp config_default.py config.py.txt
+cp wsgi.py wsgi.py.txt
 echo "built_in_server_host = '0.0.0.0'" >> config.py 
 echo "built_in_server_debug = False" >> config.py 
 echo "my_host_port = 8000" >> config.py
